6ad6ce579af26c1a7d4aec33d7bbb463b511ea59
Pedro Proni <pedro_proni@hotmail.com>
2025-04-03 11:41:00 -0300

    feat(Navbar): enhance navigation with additional links and icons

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons'
-import { faMagnifyingGlass, faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faBagShopping, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 </script>
@@ -10,18 +10,37 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
         <div class="logo">
             <img src="#" alt="Logo" />
         </div>
+        <div class="navbar-links">
+            <a>
+                Shop
+                <FontAwesomeIcon :icon="faAngleDown" style="font-size: 1.5rem;"/>
+            </a>
+            <a>
+                Home
+                <FontAwesomeIcon :icon="faAngleDown" style="font-size: 1.5rem;"/>
+            </a>
+            <a>
+                Categories
+                <FontAwesomeIcon :icon="faAngleDown" style="font-size: 1.5rem;"/>
+            </a>
+            <a>
+                Pages
+                <FontAwesomeIcon :icon="faAngleDown" style="font-size: 1.5rem;"/>
+            </a>
+        </div>
         <nav>
             <a href="#login">
-            <FontAwesomeIcon :icon="faUser" style="font-size: 2.5rem;" />
+                <FontAwesomeIcon :icon="faUser" style="font-size: 2.5rem;" />
             </a>
             <a href="#wishlist">
-            <FontAwesomeIcon :icon="faHeart" style="font-size: 2.5rem;" />
+                <FontAwesomeIcon :icon="faHeart" style="font-size: 2.5rem;" />
             </a>
             <a href="#search">
-            <FontAwesomeIcon :icon="faMagnifyingGlass" style="font-size: 2.5rem;" />
+                <FontAwesomeIcon :icon="faMagnifyingGlass" style="font-size: 2.5rem;" />
             </a>
             <a href="#cart">
-            <FontAwesomeIcon :icon="faBagShopping" style="font-size: 2.5rem;" />
+                <FontAwesomeIcon :icon="faBagShopping" style="font-size: 2.5rem;" />
+                <FontAwesomeIcon :icon="faAngleDown" style="font-size: 1.5rem; margin-left: 0.5rem;" />
             </a>
         </nav>
     </div>
@@ -41,6 +60,26 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     height: 50px;
 }
 
+.navbar {
+    padding: 0 5rem;
+}
+
+.navbar-links {
+    display: flex;
+    gap: 20px;
+}
+
+.navbar-links a {
+    color: white;
+    text-decoration: none;
+    font-size: 1.7rem;
+    transition: all 300ms ease-in-out;
+    padding: 0.1rem;
+    position: relative;
+    margin-left: 1rem;
+    cursor: pointer;
+}
+
 nav {
     display: flex;
     gap: 20px;
@@ -54,6 +93,10 @@ nav a {
     color: white;
     text-decoration: none;
     position: relative;
+    margin-left: 1rem;
+    font-size: 2.5rem;
+    transition: all 300ms ease-in-out;
+    padding: 0.1rem;
 }
 
 nav a:after {
@@ -65,10 +108,9 @@ nav a:after {
     transition: all 300ms ease-in-out;
     position: absolute;
     bottom: 1.5rem;
-    }
+}
 
 nav a:hover:after {
     width: 100%;
 }
-
 </style>
